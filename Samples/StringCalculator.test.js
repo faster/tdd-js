56022b82d115674873093904dead9e3cbfd7efca
Golovin Kirill <faster_@mail.ru>
2017-10-14 19:10:20 +0300

    some beaty

--- Samples/StringCalculator.test.js
+++ Samples/StringCalculator.test.js
@@ -1,12 +1,10 @@
-import StringCalculator from './StringCalculator';
-import chai from "chai";
-chai.should();
-//var expect = require('chai').expect;
+import StringCalculator from "./StringCalculator";
+import "chai/register-should";
 
 describe("StringCalculator", function() {
-    it('Sample one', function(){
+    it("Sample one", function(){
         var calc = new StringCalculator();
         var result = calc.add("genka");
-        result.should.eql(1);
+        result.should.be.equal(1);
     });
 });
