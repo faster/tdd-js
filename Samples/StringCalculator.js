30bb411479379a2acae669de41bb4394692b6d18
Golovin Kirill <faster_@mail.ru>
2017-10-14 22:26:43 +0300

    stringCalculator implementation and single tests (w/o test cases)

--- Samples/StringCalculator.js
+++ Samples/StringCalculator.js
@@ -1,18 +1,53 @@
 export default class StringCalculator{
 
+    /**
+     * 
+     * @param {string} expr 
+     */
     add(expr){
-        return 1;
+        var refWrapper = {text: expr};
+        var delimiters = StringCalculator.ParseDelimiters(refWrapper);
+        expr = refWrapper.text;
+        var parsedNumbers = StringCalculator.ParseNumbers(expr, delimiters);
+
+        StringCalculator.FailOnNegatives(parsedNumbers);
+        return parsedNumbers.length > 0 
+            ? parsedNumbers.reduce((a, b) => a + b)
+            : 0;  
     };
-    
+
+    /**
+     * 
+     * @param {string} expr 
+     * @param {string[]} delimiters 
+     * @returns {number[]}
+     */
     static ParseNumbers(expr, delimiters){
-
+        return expr.split(delimiters).map(Number);
     }
     
+    /**
+     * 
+     * @param {number[]} numbers 
+     */
     static FailOnNegatives(numbers){
-
+        var negatives = numbers.filter(n => n < 0);
+        if (negatives.length > 0){
+            throw new Error("negatives not allowed: " + negatives.join(", "));
+        }
     }
 
-    static ParseDelimiters(text){
-
+    /**
+    * @param {string} text The string
+    * @returns {string}
+    */
+    static ParseDelimiters(wrapper){
+        var text = wrapper.text;
+        if (!text.startsWith("//") || text.length <= 2){
+            return /[\n,]+/;
+        }
+        var delimiter = text[2];
+        wrapper.text = text.split("\n")[1];
+        return delimiter;
     }
 };
--- Samples/StringCalculator.test.js
+++ Samples/StringCalculator.test.js
@@ -1,10 +1,18 @@
 import StringCalculator from "./StringCalculator";
 import "chai/register-should";
+import "chai/register-expect";
 
 describe("StringCalculator", function() {
-    it("Sample one", function(){
-        var calc = new StringCalculator();
-        var result = calc.add("genka");
-        result.should.be.equal(1);
+    var calc = new StringCalculator();
+    
+    it("returnZero_onEmptyInput", function(){
+        var res = calc.add("");
+        res.should.be.equal(0);
+    });
+    it("throwException_onNegativeNumbers", function(){
+        expect(()=>calc.add("-1")).to.throw();
+    });
+    it("listAllNegativesInExceptionMessage_whenFail", function(){
+        expect(()=>calc.add("-1,2,-5,-100,8")).to.throw("negatives not allowed: -1, -5, -100");
     });
 });
