e62250cffe0388a401dea08bdef562494fef0936
Golovin Kirill <faster_@mail.ru>
2017-10-22 11:29:46 +0300

    review fixes

--- Samples/StringCalculator.js
+++ Samples/StringCalculator.js
@@ -1,53 +1,55 @@
-export default class StringCalculator{
+ /**
+  * 
+  * @param {string} expr 
+  * @param {string[]} delimiters 
+  * @returns {number[]}
+  */
+ function parseNumbers(expr, delimiters) {
+     return expr.split(delimiters).map(Number);
+ }
 
+ /**
+ * @param {string} text The string
+ */
+ function parseDelimiters(text) {
+     let result = {
+         delimiters: "",
+         processedText: text
+     }
+     if (text.startsWith("//") && text.length > 2) {
+         result.delimiters = text[2];
+         result.processedText = text.split("\n")[1];
+     }
+     else {
+         result.delimiters = /[\n,]+/;
+     }
+
+     return result;
+ }
+ 
+ /**
+  * 
+  * @param {number[]} numbers 
+  */
+ function failOnNegatives(numbers) {
+     var negatives = numbers.filter(n => n < 0);
+     if (negatives.length > 0){
+         throw new Error("negatives not allowed: " + negatives.join(", "));
+     }
+ }
+
+export default class StringCalculator {
     /**
      * 
      * @param {string} expr 
      */
-    add(expr){
-        var refWrapper = {text: expr};
-        var delimiters = StringCalculator.ParseDelimiters(refWrapper);
-        expr = refWrapper.text;
-        var parsedNumbers = StringCalculator.ParseNumbers(expr, delimiters);
+    add(expr) {
+        let {delimiters, processedText} = parseDelimiters(expr);
+        var parsedNumbers = parseNumbers(processedText, delimiters);
 
-        StringCalculator.FailOnNegatives(parsedNumbers);
+        failOnNegatives(parsedNumbers);
         return parsedNumbers.length > 0 
             ? parsedNumbers.reduce((a, b) => a + b)
             : 0;  
     };
-
-    /**
-     * 
-     * @param {string} expr 
-     * @param {string[]} delimiters 
-     * @returns {number[]}
-     */
-    static ParseNumbers(expr, delimiters){
-        return expr.split(delimiters).map(Number);
-    }
-    
-    /**
-     * 
-     * @param {number[]} numbers 
-     */
-    static FailOnNegatives(numbers){
-        var negatives = numbers.filter(n => n < 0);
-        if (negatives.length > 0){
-            throw new Error("negatives not allowed: " + negatives.join(", "));
-        }
-    }
-
-    /**
-    * @param {string} text The string
-    * @returns {string}
-    */
-    static ParseDelimiters(wrapper){
-        var text = wrapper.text;
-        if (!text.startsWith("//") || text.length <= 2){
-            return /[\n,]+/;
-        }
-        var delimiter = text[2];
-        wrapper.text = text.split("\n")[1];
-        return delimiter;
-    }
 };
