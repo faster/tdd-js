af05bb8d375d0efb8f481b6e3be47ebb5c0f9278
Golovin Kirill <faster_@mail.ru>
2017-10-22 11:35:39 +0300

    good test names

--- Samples/StringCalculator.test.js
+++ Samples/StringCalculator.test.js
@@ -6,40 +6,40 @@ import "chai/register-expect";
 describe("StringCalculator", () => {
     var calc = new StringCalculator();
     
-    it("returnZero_onEmptyInput", function(){
+    it("Return zero on empty input", function(){
         var res = calc.add("");
         res.should.be.equal(0);
     });
 
     test({
-        name: "sum_ComaSeparatedNumbers sum of {value} is {expected}", 
+        name: "Sum coma separated numbers of {value} is {expected}", 
         values: ["", "42", "42,13", "1,2,3,4,5"],
         expected: [0, 42, 55, 15] 
     }, calc.add);
 
     test({
-        name: "sum_NewlineSeparatedNumbers sum of {value} is {expected}", 
+        name: "Sum newline separated numbers of {value} is {expected}", 
         value: "1\n2\n3",
         expected: 6 
     }, calc.add);
 
     test({
-        name: "sum_NumbersSeparatedByBothCommaAndNewline sum of {value} is {expected}", 
+        name: "Sum numbers separated by both comma and newline of {value} is {expected}", 
         value: "1\n2,3",
         expected: 6 
     }, calc.add);
 
     test({
-        name: "sumDelimiterFromFirstSpecialLine sum of {value} is {expected}", 
+        name: "Sum delimiter from first special line of {value} is {expected}", 
         values: ["//;\n1;2;3", "//|\n4|5|61"],
         expected: [6, 70]
     }, calc.add);
 
-    it("throwException_onNegativeNumbers", function(){
+    it("Throw exception on negative numbers", function(){
         expect(()=>calc.add("-1")).to.throw();
     });
     
-    it("listAllNegativesInExceptionMessage_whenFail", function(){
+    it("List all negatives in exception message when fail", function(){
         expect(()=>calc.add("-1,2,-5,-100,8")).to.throw("negatives not allowed: -1, -5, -100");
     });
 });
